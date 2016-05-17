0396b692f39e2ef03e9f6b20d06a2a6925ca35a8
Zac Ioannidis <zacoppotamus@gmail.com>
2016-05-17 09:35:37 +0100

    Added sleep quality

--- public/app/directives/distance-summary.js
+++ public/app/directives/distance-summary.js
@@ -23,11 +23,117 @@ angular.module('summary.directives', [])
           d3.select('#summaryContainer')
             .datum(scope.summary)
             .call(sm);
+        })
+      }
+    }
+  })
+  .directive('sleepSummary', function() {
+    return {
+      link: function(scope, elm, attr) {
+        scope.$watch('summary', function(newValue, oldValue) {
+          if (newValue == oldValue) {
+            return;
+          }
+
+          d3.select('#sleepContainer').selectAll("*").remove();
+
+          var sleepContainer = d3.select(elm[0])
+            .append('div')
+            .attr('id', 'sleepContainer')
+
+          var sleepData = getSleepData();
+          console.log('sleep data', sleepData);
+          var sc = sleep().width(200).height(300);
+          d3.select('#sleepContainer')
+            .datum(sleepData)
+            .call(sc);
+
+          function getSleepData() {
+            var data = [];
+            for (var i=0; i<100; i++) {
+              if (Math.random() < 0.08) {
+                data.push({
+                  'time': i,
+                  'restless': 1
+                });
+              }
+              else {
+                data.push({
+                  'time': i,
+                  'restless': 0
+                });
+              }
+            }
+
+            return data;
+          }
 
         })
       }
     }
   })
+
+function sleep() {
+  var width = 200;
+  var height = 300;
+
+  var y = d3.scale.linear().domain([0,100]).range([0, height]);
+  var x = d3.scale.quantize().domain([0,1]).range([0, width/2]);
+
+
+  function render(selection) {
+    console.log('inside render', this);
+    selection.each(function(d, i) {
+      var line = d3.svg.line()
+       .interpolate("step-after")
+       .y(function(d) { return y(d.time)*2; })
+       .x(function(d) { return x(d.restless); });
+
+      d3.select(this).append('h1')
+        .text('Sleep Quality')
+
+      var svg = d3.select(this).append('svg')
+        .attr('height', height)
+        .attr('width', width)
+        .append('g')
+
+      svg.append('path')
+        .attr('stroke', '#fff')
+        .attr('stroke-width', '0.5')
+        .attr('opacity', 0.8)
+        .datum(d)
+        .attr('class', 'sparkline')
+        .attr('d', line);
+
+      d3.select(this).append('h2')
+        .text('Efficiency')
+
+      var restlessCount = 0;
+      _.each(d, function(d) {
+        if(d.restless == 1) {
+          restlessCount += 1;
+        }
+      });
+
+      d3.select(this).append('p')
+        .text('' + ((1 - restlessCount/100)*100) + '%');
+    })
+  }
+
+  render.width = function(value) {
+    if (!arguments.length) return width;
+    width = value;
+    return render;
+  };
+
+  render.height = function(value) {
+    if (!arguments.length) return height;
+    height = value;
+    return render;
+  };
+
+  return render;
+}
 
 function summary() {
   var width = 300,
